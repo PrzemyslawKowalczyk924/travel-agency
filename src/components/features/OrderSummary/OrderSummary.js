5470546a64981fe9367ed70cde94fa7732b2ad7f
unknown <kowalczyk.przemek@wp.pl>
2021-02-02 01:03:24 +0100

    Add trip options for increasing price

--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './OrderSummary.scss';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import { formatPrice } from '../../../utils/formatPrice';
 
-const OrderSummary = () => (
-  <h2 className={styles.component}>Total:<strong>$12.345</strong></h2>
+const OrderSummary = ({tripCost, options}) => (
+  <h2 className={styles.component}>
+    Total:<strong>{formatPrice(calculateTotal(tripCost, options))}</strong>
+  </h2>
 );
+
+OrderSummary.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
 
 export default OrderSummary;
